0c7696aeed2834a29a48a96a62eb2664a601fda3
leriashes <valeriya-shevchenko-2002@mail.ru>
2025-03-13 21:26:14 +0700

    Complete Task

--- src/main/java/ru/hh/kafkahw/internal/Service.java
+++ src/main/java/ru/hh/kafkahw/internal/Service.java
@@ -4,6 +4,9 @@ import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,15 +14,25 @@ public class Service {
 
   private final ConcurrentMap<String, ConcurrentMap<String, AtomicInteger>> counters = new ConcurrentHashMap<>();
   private final Random random = new Random();
+  private final static Logger LOGGER = LoggerFactory.getLogger(Service.class);
 
   public void handle(String topic, String message) {
-    if (random.nextInt(100) < 10) {
-      throw new RuntimeException();
-    }
-    counters.computeIfAbsent(topic, key -> new ConcurrentHashMap<>())
-        .computeIfAbsent(message, key -> new AtomicInteger(0)).incrementAndGet();
-    if (random.nextInt(100) < 2) {
-      throw new RuntimeException();
+    boolean handled = false;
+    while (!handled)
+    {
+      try {
+        if (random.nextInt(100) < 10) {
+          throw new RuntimeException();
+        }
+        counters.computeIfAbsent(topic, key -> new ConcurrentHashMap<>())
+            .computeIfAbsent(message, key -> new AtomicInteger(0)).incrementAndGet();
+        handled = true;
+        if (random.nextInt(100) < 2) {
+          throw new RuntimeException();
+        }
+      } catch (Exception e) {
+        LOGGER.error("Failed to handle message, topic: {}, message: {}", topic, message, e);
+      }
     }
   }
 
